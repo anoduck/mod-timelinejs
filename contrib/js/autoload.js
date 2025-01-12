de75c6efa7a82800fe8360ddd0c4af4ba2e68336
anoduck <9925396+anoduck@users.noreply.github.com>
2025-01-11 20:23:47 -0500

    Fixing malformed data source url

--- contrib/js/autoload.js
+++ contrib/js/autoload.js
@@ -1,15 +1,15 @@
-const tlCon = document.querySelector('#timeline-container');
-const tlObj = tlCon.querySelector('div:first-child');
-let blockId = tlObj.id;
-let tlData = tlObj.dataset.name;
-let scripts = document.getElementsByTagName('script');
-let jsPath = scripts[scripts.length-1].src;
-
-let Options ={
-    script_path: jsPath,
-    debug: false
+var Options ={
+    debug: true,
+    lang: 'en',
+    height: 650,
 };
 
-window.onload = function () {
-    window.timeline = new TL.Timeline(blockId, tlData);
-};
+document.addEventListener('DOMContentLoaded', function() {
+    var tlCon = document.querySelector('#timeline-container')
+    var tlObj = tlCon.querySelector('div:first-child')
+    var blockId = tlObj.id
+    console.log('blockID = ', blockId)
+    var tlData = tlObj.dataset.name
+    console.log('tlData = ', tlData)
+    window.timeline = new TL.Timeline(blockId, tlData, Options)
+})
--- exampleSite/content/_index.md
+++ exampleSite/content/_index.md
@@ -6,6 +6,9 @@ date: 2023-07-10
 
 __There should be a fancy Timeline element below.__
 
-{{< timelinejs blockId="timeline-element" tlData="timelinejs.json" >}}
+
+{{< timelinejs blockId="timeline-id" tlData="./timelinejs.json" >}}
+
+<!-- {{< timelinejs blockId="timeline-id" tlData="https://docs.google.com/spreadsheets/d/e/2PACX-1vSoEV-bT1CXcTsWDUX8ubyBH1hFb0DrokqJAfc66DQfpVBxv7JZKIPwZ3SzjBBFTADw4NAy-NvOtzma/pubhtml" >}} -->
 
 If there is not, then check the browser console to see if it loaded, then inspect the element to see if the element has the proper dimensions.
