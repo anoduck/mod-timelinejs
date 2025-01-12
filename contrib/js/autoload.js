b90d166c033abad295dfaf87fea4eea14a17c1ae
anoduck <9925396+anoduck@users.noreply.github.com>
2025-01-12 05:50:27 -0500

    fix: significant Improvements and Fixes
    Now truly allows both a Google Sheet and a JSON file. All rendering works well, and even added
    ability to debug the module.

--- contrib/js/autoload.js
+++ contrib/js/autoload.js
@@ -1,15 +1,26 @@
-var Options ={
-    debug: true,
-    lang: 'en',
-    height: 650,
-};
-
 document.addEventListener('DOMContentLoaded', function() {
-    var tlCon = document.querySelector('#timeline-container')
-    var tlObj = tlCon.querySelector('div:first-child')
-    var blockId = tlObj.id
-    console.log('blockID = ', blockId)
-    var tlData = tlObj.dataset.name
-    console.log('tlData = ', tlData)
-    window.timeline = new TL.Timeline(blockId, tlData, Options)
+    var tlCon = document.querySelector('#timeline-container');
+    var tlObj = tlCon.querySelector('div:first-child');
+    var blockId = tlObj.id;
+    var tlData = tlObj.dataset.name;
+    var debugData = tlObj.dataset.debug;
+
+    if (debugData == 'true') {
+        var debugOn = true
+    } else {
+        var debugOn = false
+    };
+
+    if (debugOn) {
+        console.log('blockID = ', blockId)
+        console.log('tlData = ', tlData)
+        console.log('Debug = ', debugOn)
+    };
+
+    var Options ={
+        debug: debugData,
+        lang: 'en',
+        height: 650,
+    };
+    window.timeline = new TL.Timeline(blockId, tlData, Options);
 })
